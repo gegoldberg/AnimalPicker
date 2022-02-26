a00e749ef88bc3de5ea9041c71a6cb6857ddcf10
Garrett <ggoldbergtwin@gmail.com>
2022-02-26 00:01:33 -0500

    Added option for fish art.

--- src/AnimalPicker.java
+++ src/AnimalPicker.java
@@ -6,16 +6,20 @@ public class AnimalPicker {
 		boolean choice = true;
 		while(choice) {
 		Scanner input = new Scanner(System.in);
-		System.out.println("Which animal would you like to see?\n1. Cat\n2. Dog");
+		System.out.println("Which animal would you like to see?\n1. Cat\n2. Dog\n3. Fish");
 		int userInput = input.nextInt();
 		
 		if (userInput == 1) {
-			System.out.println("\" /\\\\_/\\\\\\n( o.o )\\n > ^ <\"");
+			System.out.println(" _._     _,-\'\"\"`-._\n(,-.`._,\'(       |\\`-/|\n    `-.-\' \\ )-`( , o o)\n          `-    \\`_`\"\'-");
 			choice = false;
 		} else if (userInput == 2) {
-			System.out.println("\",-.___,-.\\n\\\\_/_ _\\\\_/\\n  )O_O(\\n { (_) }\\n  `-^-\\'   \"");
+			System.out.println("            __\n(\\,--------\'()\'--o\n (_    ___    /~\"\n  (_)_)  (_)_)");
 			choice = false;
-		} else {
+		} else if (userInput == 3) {
+			System.out.println("|\\   \\\\\\\\__     o\n| \\_/    o \\    o \n> _   (( <_  oo  \n| / \\__+___/      \n|/     |/");
+			choice = false;
+		}
+		else {
 			System.out.println("Invalid input.");
 		}
 		
